61f065c415c1186a175d4a1052bd01e046adcbec
Jakob-1870 <108411081+Jakob-1870@users.noreply.github.com>
2022-08-29 18:46:27 -0400

    Delete Airgap.sh

--- Airgap.sh
+++
@@ -1,9 +0,0 @@
-# airgapping
-sudo rm -r /lib/modules/$(uname -r)/kernel/drivers/net/ethernet # delete ethernet, wifi, and bluetooth drivers
-sudo rm -r /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-sudo rm -r /lib/modules/$(uname -r)/kernel/drivers/bluetooth
-nmcli networking off # disable internet
-sudo systemctl disable bluetooth.service --force # disable bluetooth
-sudo systemctl stop bluetooth
-
-zenity --info --text "Your computer is now software airgapped"
