4ad5aa3b8be959fb296e276f3abe004c8bde23d0
Jakob-1870 <108411081+Jakob-1870@users.noreply.github.com>
2022-08-29 18:52:41 -0400

    Update RestoreOfflineWallet.sh

--- Files/Offline_Restore/RestoreOfflineWallet.sh
+++ Files/Offline_Restore/RestoreOfflineWallet.sh
@@ -8,6 +8,7 @@ nmcli networking off # disable internet
 sudo systemctl disable bluetooth.service --force # disable bluetooth
 sudo systemctl stop bluetooth
 #------
+zenity --notification --text "Software Airgap Enabled"
 
 tar -xzf Files/bitcoin-0.21.2-x86_64-linux-gnu.tar.gz -C $HOME
 mkdir $HOME/.bitcoin $HOME/.bitcoin/wallets
@@ -17,3 +18,5 @@ wallet_number=$(pwd | tail -c 2)
 cp -r YetiWallet* $HOME/.bitcoin/wallets
 $HOME/bitcoin-0.21.2/bin/bitcoin-qt -server & sleep 2
 $HOME/bitcoin-0.21.2/bin/bitcoin-cli loadwallet "YetiWallet$wallet_number"
+
+zenity --notification --text "TURN COMPUTER OFF WHEN FINISHED"
